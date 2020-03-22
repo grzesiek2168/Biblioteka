863e17f6c9dc9d7b57782be22e3c777f7558fe0a
grzesiek2168 <Za89C49w1W>
2020-03-22 17:26:20 +0100

    Dodabue setterow i getterow

--- appBiblioteka/src/Book.java
+++ appBiblioteka/src/Book.java
@@ -70,7 +70,10 @@ public class Book {
 
     void printInfo() {
         String info = title + "; " + author + "; " + releaseDate + "; "
-                + pages + "; " + publisher + "; " + isbn;
+                + pages + "; " + publisher;
+        if(isbn!=null){
+            info=info+";"+isbn;
+        }
         System.out.println(info);
     }
 }
