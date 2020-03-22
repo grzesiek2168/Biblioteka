6eaa3d45128d23b015815c0dc993e0a7b2f2a956
grzesiek2168 <Za89C49w1W>
2020-03-22 17:22:40 +0100

    Dodabue setterow i getterow

--- appBiblioteka/src/Book.java
+++ appBiblioteka/src/Book.java
@@ -6,6 +6,53 @@ public class Book {
     String publisher;
     String isbn;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 
     public Book(String title, String author, int releaseDate,
                 int pages, String publisher, String isbn) {
