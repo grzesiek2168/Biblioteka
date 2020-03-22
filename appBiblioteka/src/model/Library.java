d0918405c55c79c0b89a69e508b81ffcd030e477
grzesiek2168 <Za89C49w1W>
2020-03-22 19:37:36 +0100

    Dodanie tablic

---
+++ appBiblioteka/src/model/Library.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Library {
+    private final int maxBooks = 1000;
+    private Book[] books = new Book[maxBooks];
+    private int booksNumber;
+
+    public void addBook(Book book) {
+        if (booksNumber < maxBooks) {
+            books[booksNumber] = book;
+            booksNumber++;
+        } else {
+            System.out.println("Maxymalna liczba ksiazek zostala osiagnieta");
+        }
+    }
+
+    public void printBooks() {
+        if (booksNumber == 0) {
+            System.out.println("Brak ksiazek");
+        }
+        for (int i = 0; i < booksNumber; i++) {
+            books[i].printInfo();
+        }
+    }
+}
+
+
+
